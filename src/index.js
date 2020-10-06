bae2f83ec3833a7cc13f4c793915a4a7cdf9db45
ktapia576 <ktapia576@hotmail.com>
2020-10-06 01:16:10 -0400

    change from function component to class component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-    return <h1> Hello, {props.name}</h1>;
+class Welcome extends React.Component {
+    render () {
+        return <h1> Hello, {this.props.name}</h1>;
+    }
 }
 
 const element = <Welcome name='Sara' />;
