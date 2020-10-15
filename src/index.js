e1de16d17bfc3ac7c947c6b504c5456a60c84723
ktapia576 <ktapia576@hotmail.com>
2020-10-15 18:57:54 -0400

    Add calculator component with boiling verdict function component to have user type whether or not temp wil boil water

--- src/index.js
+++ src/index.js
@@ -254,6 +254,39 @@ class EssayForm extends React.Component {
     }
 }
 
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;  
+    }
+    return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />        
+                <BoilingVerdict
+                    celsius={parseFloat(temperature)} />      
+            </fieldset>
+
+        );
+    }
+}
 
 class App extends React.Component {
     render () {
@@ -273,6 +306,8 @@ class App extends React.Component {
                 />
                 <Page />
                 <EssayForm />
+                <br/>
+                <Calculator />
             </div>
         );
     }
