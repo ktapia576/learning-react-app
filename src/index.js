0ecc0f233885de6eae5177d6a4d9e59e36346872
ktapia576 <ktapia576@hotmail.com>
2020-10-15 19:32:40 -0400

    Add helper functions to convert

--- src/index.js
+++ src/index.js
@@ -259,6 +259,25 @@ const scaleNames = {
     f: 'Fahrenheit'
 };
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+  
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
 function BoilingVerdict(props) {
     if (props.celsius >= 100) {
         return <p>The water would boil.</p>;  
@@ -285,9 +304,7 @@ class TemperatureInput extends React.Component {
                 <legend>Enter temperature in {scaleNames[scale]}:</legend>
                 <input
                     value={temperature}
-                    onChange={this.handleChange} />        
-                <BoilingVerdict
-                    celsius={parseFloat(temperature)} />      
+                    onChange={this.handleChange} />         
             </fieldset>
         );
     }
